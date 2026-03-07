e09a3bf04306ec8f411c110a765f06134d81a16a
Trex <deltamelter@users.noreply.github.com>
2026-03-07 21:42:51 +0000

    Update ef_ps.h

--- components/ef_ps/ef_ps.h
+++ components/ef_ps/ef_ps.h
@@ -1,44 +1,43 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/canbus/canbus.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace ef_ps {
 
-// Change PollingComponent to esphome::PollingComponent to be explicit
-class EfPs : public esphome::PollingComponent, public esphome::canbus::CanbusListener {
+class EfPs : public PollingComponent, public canbus::CanbusListener {
  public:
   void setup() override;
   void update() override;
-  void dump_config() override; // Added this declaration
-  
-  // This must match the signature in esphome/components/canbus/canbus.h
-  void on_frame(const esphome::canbus::CanFrame &frame) override;
+  void dump_config() override;
+  void on_frame(const canbus::CanFrame &frame) override;
 
-  void set_canbus_id(esphome::canbus::Canbus *canbus) { this->canbus_ = canbus; }
+  void set_canbus_id(canbus::Canbus *canbus) { this->canbus_ = canbus; }
 
-  // Setters for sensors (keep these from previous step)
-  void set_battery_soc_sensor(sensor::Sensor *s) { this->battery_soc_sensor_ = s; }
-  void set_battery_voltage_sensor(sensor::Sensor *s) { this->battery_voltage_sensor_ = s; }
-  // ... (include other sensor setters here)
+  // Sensor Setters
+  void set_battery_soc_sensor(sensor::Sensor *s) { battery_soc_sensor_ = s; }
+  void set_battery_voltage_sensor(sensor::Sensor *s) { battery_voltage_sensor_ = s; }
+  void set_battery_current_sensor(sensor::Sensor *s) { battery_current_sensor_ = s; }
 
-  // Data methods
+  // Data Setters (Lambdas)
   void set_battery_soc(float val);
   void set_battery_voltage(float val);
+  void set_battery_current(float val);
 
  protected:
-  esphome::canbus::Canbus *canbus_;
+  canbus::Canbus *canbus_{nullptr};
   
   float battery_soc_{0};
   float battery_voltage_{0};
+  float battery_current_{0};
 
   sensor::Sensor *battery_soc_sensor_{nullptr};
   sensor::Sensor *battery_voltage_sensor_{nullptr};
+  sensor::Sensor *battery_current_sensor_{nullptr};
 
-  void send_can_heartbeat();
-  void send_can_battery_info();
+  void send_can_data(uint32_t id, const std::vector<uint8_t> &data);
 };
 
 }  // namespace ef_ps
