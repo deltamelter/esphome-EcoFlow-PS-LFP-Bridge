61a7c23aa989725397841d457a1fd4f05092ffd2
Trex <deltamelter@users.noreply.github.com>
2026-03-07 21:40:16 +0000

    Update __init__.py

--- components/ef_ps/__init__.py
+++ components/ef_ps/__init__.py
@@ -3,15 +3,11 @@ import esphome.config_validation as cv
 from esphome.components import canbus
 from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
 
-# Add this line to properly reference the CanbusListener class
-CANBUS_LISTENER_CLASS = canbus.canbus_ns.class_("CanbusListener")
-
 DEPENDENCIES = ["canbus"]
-AUTO_LOAD = ["canbus"]
+AUTO_LOAD = ["canbus", "sensor"]
 
 ef_ps_ns = cg.esphome_ns.namespace("ef_ps")
-# Use cg.PollingComponent and our new CANBUS_LISTENER_CLASS
-EfPs = ef_ps_ns.class_("EfPs", cg.PollingComponent, CANBUS_LISTENER_CLASS)
+EfPs = ef_ps_ns.class_("EfPs", cg.PollingComponent, canbus.CanbusListener)
 
 CONF_CANBUS_ID = "canbus_id"
 
@@ -28,5 +24,4 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     can = await cg.get_variable(config[CONF_CANBUS_ID])
-    # Ensure this matches the method name in your ef_ps.h
     cg.add(var.set_canbus_id(can))
