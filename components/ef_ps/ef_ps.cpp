46192320c63aa159203bfeb5e7390f57e80e1bf6
Trex <deltamelter@users.noreply.github.com>
2026-03-08 01:56:21 +0000

    Update ef_ps.cpp

--- components/ef_ps/ef_ps.cpp
+++ components/ef_ps/ef_ps.cpp
@@ -8,8 +8,8 @@ static const char *const TAG = "ef_ps";
 
 void EfPs::setup() {
   if (this->canbus_ != nullptr) {
-    // This registers our component to receive CAN frames
-    this->canbus_->add_driver(this);
+    // The compiler confirmed this is the correct method name
+    this->canbus_->add_trigger(this);
   }
 }
 
@@ -17,8 +17,8 @@ void EfPs::dump_config() {
   ESP_LOGCONFIG(TAG, "EcoFlow PS Bridge");
 }
 
-void EfPs::on_frame(const ::esphome::canbus::CanFrame &frame) {
-  // Logic to handle incoming data from PowerStream
+void EfPs::on_frame(const canbus::CanFrame &frame) {
+  // Logic for incoming frames
 }
 
 void EfPs::set_battery_soc(float val) {
@@ -35,27 +35,19 @@ void EfPs::set_battery_voltage(float val) {
 void EfPs::send_can_data() {
   if (this->canbus_ == nullptr) return;
 
-  // 0x621 Battery Status Frame
-  ::esphome::canbus::CanFrame bat_frame{};
-  bat_frame.can_id = 0x621;
-  bat_frame.use_extended_id = true;
-  bat_frame.can_dlc = 8;
-  
+  // 0x621 Battery Status
+  // send_data(id, use_extended, data_vector)
   uint16_t volt_scaled = (uint16_t)(this->battery_voltage_ * 100);
-  bat_frame.data[0] = volt_scaled & 0xFF;
-  bat_frame.data[1] = (volt_scaled >> 8) & 0xFF;
-  bat_frame.data[4] = (uint8_t)this->battery_soc_;
+  std::vector<uint8_t> bat_data(8, 0);
+  bat_data[0] = volt_scaled & 0xFF;
+  bat_data[1] = (volt_scaled >> 8) & 0xFF;
+  bat_data[4] = (uint8_t)this->battery_soc_;
   
-  this->canbus_->send_frame(bat_frame);
+  this->canbus_->send_data(0x621, true, bat_data);
 
-  // 0x2FF Heartbeat Frame
-  ::esphome::canbus::CanFrame hb_frame{};
-  hb_frame.can_id = 0x2FF;
-  hb_frame.use_extended_id = true;
-  hb_frame.can_dlc = 8;
-  hb_frame.data[0] = 0x01;
-  
-  this->canbus_->send_frame(hb_frame);
+  // 0x2FF Heartbeat
+  std::vector<uint8_t> hb_data = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  this->canbus_->send_data(0x2FF, true, hb_data);
 }
 
 }  // namespace ef_ps
