eaafe94a6ee08a7b985e149eb1c19fe62ec17cff
Trex <deltamelter@users.noreply.github.com>
2026-03-08 01:30:06 +0000

    Update ef_ps.h

--- components/ef_ps/ef_ps.h
+++ components/ef_ps/ef_ps.h
@@ -7,40 +7,32 @@
 namespace esphome {
 namespace ef_ps {
 
-// Only inherit from CanbusTrigger. It already includes 'Component'.
-class EfPs : public esphome::canbus::CanbusTrigger {
+class EfPs : public Component, public canbus::CanbusListener {
  public:
   void setup() override;
   void dump_config() override;
   
-  // This is the function called when a CAN frame matches (if configured) 
-  // or via the listener logic.
-  void on_frame(const esphome::canbus::CanFrame &frame) override;
+  // This is the exact signature required by CanbusListener
+  void on_frame(const canbus::CanFrame &frame) override;
 
-  void set_canbus_id(esphome::canbus::Canbus *canbus) { this->canbus_ = canbus; }
+  void set_canbus_id(canbus::Canbus *canbus) { this->canbus_ = canbus; }
   void set_update_interval(uint32_t interval) { this->update_interval_ = interval; }
 
-  // Sensor Setters
   void set_battery_soc_sensor(sensor::Sensor *s) { battery_soc_sensor_ = s; }
   void set_battery_voltage_sensor(sensor::Sensor *s) { battery_voltage_sensor_ = s; }
-  void set_battery_current_sensor(sensor::Sensor *s) { battery_current_sensor_ = s; }
 
-  // Data Setters
   void set_battery_soc(float val);
   void set_battery_voltage(float val);
-  void set_battery_current(float val);
 
  protected:
-  esphome::canbus::Canbus *canbus_{nullptr};
+  canbus::Canbus *canbus_{nullptr};
   uint32_t update_interval_{1000};
   
   float battery_soc_{0};
   float battery_voltage_{0};
-  float battery_current_{0};
 
   sensor::Sensor *battery_soc_sensor_{nullptr};
   sensor::Sensor *battery_voltage_sensor_{nullptr};
-  sensor::Sensor *battery_current_sensor_{nullptr};
 
   void send_can_data();
 };
