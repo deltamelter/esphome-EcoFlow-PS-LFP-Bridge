961f53ac3ce1a092e578aa7f6b6d78ca008a3393
Trex <deltamelter@users.noreply.github.com>
2026-03-08 01:56:53 +0000

    Update __init__.py

--- components/ef_ps/__init__.py
+++ components/ef_ps/__init__.py
@@ -1,30 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import canbus
-from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["canbus"]
 AUTO_LOAD = ["canbus", "sensor"]
 
 ef_ps_ns = cg.esphome_ns.namespace("ef_ps")
-
-# Match the C++ inheritance
-CanbusListener = canbus.canbus_ns.class_("CanbusListener")
-EfPs = ef_ps_ns.class_("EfPs", cg.Component, CanbusListener)
+# Match C++ header
+CanbusTrigger = canbus.canbus_ns.class_("CanbusTrigger")
+EfPs = ef_ps_ns.class_("EfPs", cg.Component, CanbusTrigger)
 
 CONF_CANBUS_ID = "canbus_id"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(EfPs),
-        cv.Required(CONF_CANBUS_ID): cv.use_id(canbus.CanbusComponent),
-        cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.update_interval,
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(EfPs),
+    cv.Required(CONF_CANBUS_ID): cv.use_id(canbus.CanbusComponent),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
     can = await cg.get_variable(config[CONF_CANBUS_ID])
     cg.add(var.set_canbus_id(can))
